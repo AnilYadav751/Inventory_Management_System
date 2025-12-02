3f1bccf3de8b1e50d03c140e753d3464c94082e8
AnilYadav751 <anilyad292@gmail.com>
2025-12-03 01:57:52 +0530

    Change database password placeholder
    Updated the database password placeholder for security.

--- src/main/java/org/example/DBConnection.java
+++ src/main/java/org/example/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/INVENTORY_DB";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "Anil@751"; // TODO: Change this
+    private static final String PASSWORD = "enter_your_password_here";        // Change This
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
